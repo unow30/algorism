faa2ebc7006ce07db85c677e91e0e9989e2d1cd4
unow30 <yunho7205@gmail.com>
2021-04-04 15:16:36 +0900

    refactor q1.js, modify q4.sql, q4.md

--- 2104/210403/2021 Dev_Matching_back_q1.js
+++ 2104/210403/2021 Dev_Matching_back_q1.js
@@ -32,14 +32,44 @@ function solution(lottos, win_nums) {
     console.log(answer);
 }
 
-solution([1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 6])//[1,1]
-solution([5, 0, 21, 17, 0, 30], [4, 5, 17, 22, 15, 20])//[3,5]
-solution([0, 0, 0, 0, 0, 6], [14, 45, 12, 29, 19, 6])//[1,6]
-solution([0, 0, 0, 0, 0, 5], [14, 45, 12, 29, 19, 6])//[2,6]
+// solution([1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 6])//[1,1]
+// solution([5, 0, 21, 17, 0, 30], [4, 5, 17, 22, 15, 20])//[3,5]
+// solution([0, 0, 0, 0, 0, 6], [14, 45, 12, 29, 19, 6])//[1,6]
+// solution([0, 0, 0, 0, 0, 5], [14, 45, 12, 29, 19, 6])//[2,6]
 
 /*
  마지막 solution의 경우의 수를 맞추지 못해 계속 고민했다.
  구분할 수 있는 lottos 번호가 win_nums와 하나도 맞지 않는 경우를 고려해야 했다.
  이 경우 correct_nums는 빈 배열이 되며 [최고등수, undefined]가 나오게 된다.
  correct_nums.length가 0인 경우에 리턴하는 경로를 만들어 문제를 해결했다.
+
+ correct_nums도 맞춘 숫자의 개수로 지정한다면 더 간결해진다.
 */
+function solution2(lottos, win_nums) {
+    var answer = [];
+    //최고순위와 최저 순위를 배열로 나타내야 한다.
+    //0은 알 수 없는 값이다. 두 변수 길이는 6이다.
+    //가장 높은 경우, 가장 낮은 경우를 찾아라.
+
+    let obj = { "0": 6, "1": 6, "2": 5, "3": 4, "4": 3, "5": 2, "6": 1 } //"당첨번호 개수": 등수
+    let correct_nums = 0 //맞춘 번호 개수
+    let zero_nums = 0 //지워진 당첨번호 개수
+
+    lottos.forEach(el => {
+        if (win_nums.includes(el)) correct_nums++
+        if (el === 0) zero_nums++
+    })
+
+    // console.log(zero_nums)
+    // console.log(correct_nums)
+
+    answer.push(obj[correct_nums + zero_nums])
+    answer.push(obj[correct_nums])
+
+    console.log(answer);
+}
+
+solution2([1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 6])//[1,1]
+solution2([5, 0, 21, 17, 0, 30], [4, 5, 17, 22, 15, 20])//[3,5]
+solution2([0, 0, 0, 0, 0, 6], [14, 45, 12, 29, 19, 6])//[1,6]
+solution2([0, 0, 0, 0, 0, 5], [14, 45, 12, 29, 19, 6])//[2,6]
